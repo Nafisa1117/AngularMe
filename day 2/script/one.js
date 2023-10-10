ac2b2dc1f8d5f8c2938bd4e7ddae1c3f441fa631
Nafisa Parveen <103953608+Nafisa1117@users.noreply.github.com>
2023-10-11 00:36:19 +0530

    day 2 - live class

---
+++ day 2/script/one.js
@@ -0,0 +1,20 @@
+"use strict";
+// let message: (string | number | boolean) = "Welcome Nafisa, hehe its me";
+// message = 555;
+//alert(message);
+//let message : any = "hehe it me Nafisa";
+//alert(message);
+// let message:Array<string> = ["Nafisa Array","Ohh Doodles","Nice me"]
+// alert(message[Math.floor(Math.random()*message.length-1)]);
+//enum Power {weak, recovering, strong};
+// enum Power {weak =1, recovering, strong};
+// console.log(Power.weak);
+// let heroPower: Power = Power.recovering;
+// console.log(heroPower);
+// let powerString = Power[heroPower];
+// console.log(powerString);
+function adder(num1, num2) {
+    return num1 + num2;
+}
+;
+console.log(adder(1, 2));
